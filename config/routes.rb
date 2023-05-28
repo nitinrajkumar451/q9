20a4cf07616231aa4cfff744ecfc881045f326e6
nitin <nitin@incubyte.co>
2023-05-28 13:22:16 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :blog_posts do
+    resource :cover_image, only: [:destroy], module: :blog_posts
+  end
+  # get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  # get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  # patch "/blog_posts/:id", to: "blog_posts#update"
+  # delete "/blog_posts/:id", to: "blog_posts#destroy"
+  # get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
+  # post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+
+  # Defines the root path route ("/")
+  root "home#index"
+  # get "about", to: "about#index", as: "about"
+  # get '/blog_posts', to: 'blog_posts#index'
+
+end
